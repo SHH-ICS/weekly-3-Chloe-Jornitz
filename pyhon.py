c05bf0e43cba54be07cbf39d4486b425c7ed5e1d
Chloe-Jornitz <159150248+Chloe-Jornitz@users.noreply.github.com>
2024-04-09 12:56:54 +0000

    finish python

--- pyhon.py
+++ pyhon.py
@@ -1,30 +1,34 @@
-while (1)
-import locale 
-locale.setlocale(locale.LC_ALL, '')
-print ("Welcome To Pizza Cost Calculator!")
-size = input("What size pizza do you want to order?")
-if size in ("large", 'Large'):
-  pizza = 6
-if size in ("extra large", "Extra Large", "Extra large", "extra Large", "extralarge", "Extralarge", "extra-large", "Extra-Large", "Extra-large"):
-  pizza = 10
-toppingnum = input("How many toppings do you want?")
-if toppingnum in ("one","1"):
-  toppings = 1
-if toppingnum in ("two","2"):
-  toppings = 1.75
-if toppingnum in ("three","3"):
-  toppings = 2.5
-if toppingnum in ("four","4"):
-  toppings = 3.35
-else:
-  print ("Error!")
-  print ("Please chose one of the size options \"Large\"or \"Extra Large\".")
-  print ("Please chose an amount of toppings that is a whole number between 1 and 4.")
-  break
-subtotal = (pizza+toppings)
-HST = 0.13*subtotal
-print("Cost:")
-print("Pizza:", locale.currency(pizza))
-print("Toppings:", locale.currency(toppings))
-print("HST:", locale.currency(HST))
-print("Total Cost:", locale.currency(HST+subtotal))
+while True:
+  import locale 
+  locale.setlocale(locale.LC_ALL, '')
+  print ("Welcome To Pizza Cost Calculator!")
+  size = input("What size pizza do you want to order?")
+  if size in ("large", "Large"):
+    pizza = 6
+  elif size in ("extra large", "Extra Large", "Extra large", "extra Large", "extralarge", "Extralarge", "extra-large", "Extra-Large", "Extra-large"):
+    pizza = 10
+  else:
+    print ("Error!")
+    print ("Please chose one of the size options \"Large\"or \"Extra Large\".")
+    break
+  toppingnum = input("How many toppings do you want?")
+  if toppingnum in ("one","1"):
+    toppings = 1
+  elif toppingnum in ("two","2"):
+    toppings = 1.75
+  elif toppingnum in ("three","3"):
+    toppings = 2.5
+  elif toppingnum in ("four","4"):
+    toppings = 3.35
+  else:
+    print ("Error!")
+    print ("Please chose an amount of toppings that is a whole number between 1 and 4.")
+    break
+  subtotal = (pizza+toppings)
+  HST = 0.13*subtotal
+  print ("Cost:")
+  print ("Pizza:", locale.currency(pizza))
+  print ("Toppings:", locale.currency(toppings))
+  print ("HST:", locale.currency(HST))
+  print ("Total Cost:", locale.currency(HST+subtotal))
+  print ("Thankyou for your order!")
